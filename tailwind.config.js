115342c68b01cb8308ab16a69d65a095e1a64ae5
jimmysza <jaimeperez2014.jp81@gmail.com>
2025-03-31 00:54:33 -0500

    Creando pagina de buscador toures con mapa y sticky

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   content: [
-    "./src/**/*.{astro,js,jsx,ts,tsx}",
+    "./src/**/*.{astro,js,jsx,ts,tsx,html}",
+    "./src/pages_other/**/*.html" // Asegura que Tailwind escanee esta carpeta
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
